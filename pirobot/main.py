30d6f8cb944cccc2147ea348c0f2977338dcfed4
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2023-11-18 10:22:16 +0530

    Update main.py

--- pirobot/main.py
+++ pirobot/main.py
@@ -1,22 +1,17 @@
-from pirobot.pirobot import run_bot
 import sys
+from pirobot.pirobot import run_bot, print_help
 
-def print_help():
-    print("pirobot - Telegram Auto Reply, When offline")
-    print("Author: HK4CRPRASAD (GitHub: https://github.com/hk4crprasad/pirobot)")
-    print("Usage:")
-    print("  pirobot --piro/-piro   : Show this help message")
-    print("  pirobot -r/-run  : Run the Telegram bot")
-    sys.exit()
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) > 1:
         arg = sys.argv[1]
-        if arg in ['--piro', '-piro']:
-            print_help()
-        elif arg in ['-r', '--run']:
+        if arg in ['-r', '--run']:
             run_bot()
+        elif arg in ['--piro', '-piro']:
+            print_help()
         else:
             print("Invalid argument. Use --piro/-piro for help or -r/--run to run the bot.")
     else:
         print("Invalid arguments. Use --piro/-piro for help or -r/--run to run the bot.")
+
+if __name__ == "__main__":
+    main()
